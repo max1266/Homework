86f0cace4090b4338e70fe9ac5a0541a63b41afe
Maxim Shmidt <history1266@mail.ru>
2022-12-04 02:05:41 +0700

    Задача 15

---
+++ zadanie15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("введите цифру от 1 до 7");
+double a = Convert.ToDouble(Console.ReadLine());
+    if((a == 1) || (a == 2) || (a == 3) || (a == 4) || (a == 5)){
+        Console.WriteLine("no");
+    }
+    else{
+        if((a == 6) || (a == 7)){
+            Console.WriteLine("yes");
+        }
+        else
+        Console.WriteLine("введите другую цифру");
+    }
+    
