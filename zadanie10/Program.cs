13ac58f2b56c912479d8da747ee548ea43432f3e
Maxim Shmidt <history1266@mail.ru>
2022-12-04 14:45:03 +0700

    задание 10

---
+++ zadanie10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a > 99){
+    int result1 =a / 10;
+    int result =  result1 % 10;
+    Console.WriteLine(result);
+    
+}
+else
+Console.WriteLine("неверное число, попробуйте заново");
