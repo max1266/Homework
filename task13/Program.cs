c323542c307d60794ca204b9a3995e9669d6ba95
Maxim Shmidt <history1266@mail.ru>
2022-12-04 20:48:38 +0700

    задание 13

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a <99){
+    Console.WriteLine("нет третьего числа");
+}
+while(a > 999){
+    a = a / 10;
+}
+a = a % 10;
+Console.WriteLine(a);
